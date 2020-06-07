4c20d181a80c4758c2c674b6117762826f728c37
Minke Zhang <minke.zhang@gmail.com>
2020-06-06 22:51:20 -0700

    Add support for multiple heuristics

--- event_spread.py
+++ event_spread.py
@@ -1,7 +1,14 @@
-from typing import Any, List, Tuple, NamedTuple
+from typing import Any, Dict, List, Tuple, NamedTuple
 
+import collections
 import dataclasses
 import datetime
+import enum
+
+
+class Heuristic(enum.Enum):
+  HEURISTIC_UNDEFINED = 0
+  HEURISTIC_MORALITY = 1
 
 
 class Pos(NamedTuple):
@@ -12,7 +19,7 @@ class Pos(NamedTuple):
 class _Event(object):
   pos: Pos
   timestamp: datetime.datetime
-  influence: float
+  influence: Dict[Heuristic, float]
   spread_rate: float
 
 
@@ -22,7 +29,7 @@ Event = dataclasses.dataclass(_Event)
 class _Global(object):
   dim: Pos
   events: List[Event]
-  influence: int
+  influence: Dict[Heuristic, float]
 
 
 Global = dataclasses.dataclass(_Global)
@@ -31,9 +38,10 @@ Global = dataclasses.dataclass(_Global)
 def GetEventInfluence(
     e: Event,
     pos: Pos,
-    timestamp: datetime.datetime) -> float:
+    timestamp: datetime.datetime,
+    heuristic: Heuristic) -> float:
   d = ((pos.x - e.pos.x) ** 2 + (pos.y - e.pos.y) ** 2) ** .5
-  return e.influence if (
+  return e.influence[heuristic] if (
       (timestamp - e.timestamp).total_seconds() * e.spread_rate >= d
   ) else 0
 
@@ -41,57 +49,51 @@ def GetEventInfluence(
 def GetInfluence(
     g: Global,
     pos: Pos,
-    timestamp: datetime.datetime) -> float:
-  return g.influence + sum(
-      [GetEventInfluence(e, pos, timestamp) for e in g.events]
+    timestamp: datetime.datetime,
+    heuristic: Heuristic) -> float:
+  return g.influence[heuristic] + sum(
+      [GetEventInfluence(e, pos, timestamp, heuristic) for e in g.events]
   )
-
-
-def UpdateGlobalEvents(
-    g: Global,
-    timestamp: datetime.datetime) -> None:
-  all_influence = {
-      e: GetEventInfluence(
-          e,
-          Pos(x=(g.dim.x * 2), y=(g.dim.y * 2)),
-          timestamp
-      ) for e in g.events
-  }
-  g.events = [e for e in all_influence if all_influence[e]]
-  g.influence += sum(all_influence.values())
 
 
 if __name__ == '__main__':
   early_event = Event(
       pos=Pos(x=10, y=10),
       timestamp=datetime.datetime.fromtimestamp(0) + datetime.timedelta(seconds=10),
-      influence=50,
+      influence=collections.defaultdict(
+          float,
+          {Heuristic.HEURISTIC_MORALITY: 50},
+      ),
       spread_rate=1,
   )
   late_event = Event(
       pos=Pos(x=10, y=10),
       timestamp=datetime.datetime.fromtimestamp(0) + datetime.timedelta(seconds=20),
-      influence=50,
+      influence=collections.defaultdict(
+          float,
+          {Heuristic.HEURISTIC_MORALITY: 50},
+      ),
       spread_rate=1,
   )
 
   g = Global(
     dim=Pos(x=10, y=10),
     events=[early_event, late_event],
-    influence=0,
+    influence=collections.defaultdict(float),
   )
 
   assert GetEventInfluence(
-      early_event, early_event.pos, early_event.timestamp
-  ) == early_event.influence
+      early_event, early_event.pos, early_event.timestamp, Heuristic.HEURISTIC_MORALITY
+  ) == early_event.influence[Heuristic.HEURISTIC_MORALITY]
 
   assert GetEventInfluence(
-      early_event, Pos(0, 0), early_event.timestamp) == 0
+      early_event, Pos(0, 0), early_event.timestamp, Heuristic.HEURISTIC_MORALITY) == 0
 
   assert GetEventInfluence(
       early_event,
       Pos(g.dim.x * 2, g.dim.y * 2),
       early_event.timestamp + datetime.timedelta(
           seconds=(max(g.dim.x, g.dim.y) * 2 ** .5 / early_event.spread_rate)
-      )
-  ) == early_event.influence
+      ),
+      Heuristic.HEURISTIC_MORALITY,
+  ) == early_event.influence[Heuristic.HEURISTIC_MORALITY]
