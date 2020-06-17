252f87c2d4806754e1cc46e59e6125203af306d7
Minke Zhang <minke.zhang@gmail.com>
2020-06-16 22:33:32 -0700

    Add simple test for AddEvent call

--- lib/core/spread.go
+++ lib/core/spread.go
@@ -2,26 +2,45 @@ package spread
 
 import (
 	"context"
+	"sync"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/proto"
 
 	espb "github.com/cripplet/event_spread/lib/proto/event_spread_go_proto"
 )
 
-type EventSpreadService struct {}
+type EventSpreadService struct {
+	eventsMux sync.Mutex
+	events []*espb.Event
+}
 
 func (s *EventSpreadService) AddEvent(ctx context.Context, req *espb.AddEventRequest) (*espb.AddEventResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "AddEvent has not been implemented")
+	s.eventsMux.Lock()
+	defer s.eventsMux.Unlock()
+
+	if req.GetEvent() == nil {
+		return nil, status.Error(codes.InvalidArgument, "cannot specify an empty Event to add to event queue")
+	}
+
+	s.events = append(s.events, proto.Clone(req.GetEvent()).(*espb.Event))
+	return &espb.AddEventResponse{}, nil
 }
 
 func (s *EventSpreadService) GetEventSpread(ctx context.Context, req *espb.GetEventSpreadRequest) (*espb.GetEventSpreadResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "GetEventSpread has not been implemented")
 }
 
+func NewEventSpreadService() (*EventSpreadService, error) {
+	return &EventSpreadService{
+	}, nil
+}
+
 func NewServer() (*grpc.Server, error) {
+	service, _ := NewEventSpreadService()
 	s := grpc.NewServer()
-	espb.RegisterEventSpreadServiceServer(s, &EventSpreadService{})
+	espb.RegisterEventSpreadServiceServer(s, service)
 	return s, nil
 }
---
+++ lib/core/spread_test.go
@@ -0,0 +1,41 @@
+package spread
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+
+	espb "github.com/cripplet/event_spread/lib/proto/event_spread_go_proto"
+)
+
+func TestAddNullEvent(t *testing.T) {
+	s, _ := NewEventSpreadService()
+	_, err := s.AddEvent(context.Background(), &espb.AddEventRequest{})
+	if err == nil {
+		t.Error("unexpectedly succeeded in adding empty Event to event queue")
+	}
+}
+
+func TestAddEvent(t *testing.T) {
+	s, _ := NewEventSpreadService()
+	ts, _ := ptypes.TimestampProto(time.Now())
+	_, err := s.AddEvent(context.Background(), &espb.AddEventRequest{
+		Event: &espb.Event{
+			Position: &espb.Position{X: 0, Y: 0},
+			Timestamp: ts,
+			Heuristics: []*espb.HeuristicValue{
+				&espb.HeuristicValue{
+					Heuristic: espb.Heuristic_HEURISTIC_MORALITY,
+					Value: 0,
+				},
+			},
+			SpreadType: espb.SpreadType_SPREAD_TYPE_SIMPLE_LINEAR,
+			SpreadRate: 0,
+		},
+	})
+	if err != nil {
+		t.Errorf("unexpectedly received error when adding Event: %v", err)
+	}
+}
